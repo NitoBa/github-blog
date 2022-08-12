cee17c9f9d26395f8f7a4b0f76065f21b8521da7
Bruno Alves <bruno@sthorm.io>
2022-08-12 16:38:04 -0300

    feat: show description

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,16 +1,11 @@
 import hljs from 'highlight.js/lib/core'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { DetailHeader } from '../../components/DetailHeader'
 import 'highlight.js/styles/github.css'
 import javascript from 'highlight.js/lib/languages/javascript'
 import markdown from 'highlight.js/lib/languages/markdown'
 
-import {
-  Container,
-  ContentPostCode,
-  ContentPostContainer,
-  ContentPostText,
-} from './styles'
+import { Container, ContentPostContainer, ContentPostText } from './styles'
 import { usePostDetail } from '../../hooks/usePostDetail'
 import { useParams } from 'react-router-dom'
 import { Loading } from '../../components/Loading'
@@ -20,31 +15,10 @@ hljs.registerLanguage('markdown', markdown)
 export function Details() {
   const { id } = useParams()
   const { loading, post, getPostDetail } = usePostDetail()
-  const [code, setCode] = useState('')
-
-  function extractCodeFromDescription(description: string) {
-    const regex = /^```(?:js|javascript|dart|python|ts)\n([\s\S]*?)```$/gm
-    const newRe = new RegExp(regex)
-    const match = newRe.exec(description)
-    if (match) {
-      console.log(match[1])
-      setCode(match[0])
-      console.log(match[0])
-      return match[1]
-    }
-    return ''
-  }
 
   useEffect(() => {
     if (id) getPostDetail(+id)
   }, [])
-
-  useEffect(() => {
-    if (post) {
-      extractCodeFromDescription(post.description)
-    }
-  }, [post])
-
   return (
     <Container>
       {loading ? (
@@ -61,15 +35,9 @@ export function Details() {
                 createdAt={post?.createdAt}
               />
               <ContentPostContainer>
-                {/* <ContentPostText>
-                  {post?.description.replaceAll(
-                    /^```(?:js|javascript|dart|python|ts)\n([\s\S]*?)```$/g,
-                    '',
-                  )}
-                </ContentPostText> */}
-                <ContentPostCode
+                <ContentPostText
                   dangerouslySetInnerHTML={{
-                    __html: hljs.highlight(code, {
+                    __html: hljs.highlight(post.description, {
                       language: 'markdown',
                     }).value,
                   }}
